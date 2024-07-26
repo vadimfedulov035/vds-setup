ed7d558067a3e479a27237e42e50f23fbf6b73bf
Vadim <vadimfedulov035@gmail.com>
2024-07-26 01:28:23 +0000

    1.0 hf 3

--- setup.sh
+++ setup.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-user="veotri"
-name="Vadim"
-email="vadimfedulov035@gmail.com"
+USER="veotri"
+NAME="Vadim"
+EMAIL="vadimfedulov035@gmail.com"
 
 set_ufw() {
-	echo "UFW setup..."
-        ufw default deny incoming
-        ufw default allow outgoing
-        ufw allow OpenSSH
-        ufw allow http
-        ufw allow https
+    echo "UFW setup..."
+    ufw default deny incoming
+    ufw default allow outgoing
+    ufw allow OpenSSH
+    ufw allow http
+    ufw allow https
 }
 
 set_swap() {
@@ -22,32 +22,41 @@ set_swap() {
                 chmod 600 /swap.img
                 mkswap /swap.img
                 swapon /swap.img
-                sysctl vm.swappiness=200
+                sysctl vm.swappiness=100
         fi
-	d="vm\.swappiness=[0-9]*"  # default value
-	setting="vm\.swappiness=200"
+	d="vm\.swappiness=[0-9]*"
+	setting="vm\.swappiness=100"
 	settings="/etc/sysctl.conf"
 	grep -q "^$setting" "$settings" || sed -i "s/$d/$setting/g" "$settings"
-	sysctl -p
 }
 
 set_journald() {
 	echo "Jouarnald setup..."
-        setting="SystemMaxUse=100M"
-        settings="/etc/systemd/journald.conf"
-        grep -q "^$setting" "$settings" || echo "$setting" >> "$settings"
+    setting="SystemMaxUse=100M"
+    settings="/etc/systemd/journald.conf"
+    grep -q "^$setting" "$settings" || echo "$setting" >> "$settings"
 }
 
 set_git() {
 	echo "Git setup..."
 	git config --global init.defaultBranch main
-	git config --global user.name $name
-	git config --global user.email $email
+	git config --global user.name $NAME
+	git config --global user.email $EMAIL
+    V_LINK="https://api.github.com/repos/jesseduffield/lazygit/releases/latest"
+    VERSION=$(curl -s $V_LINK | grep -Po '"tag_name": "v\K[^"]*')
+    ARC="lazygit_${VERSION}_Linux_x86_64.tar.gz"
+    ARC_PATH="releases/latest/download/$ARC" 
+    LINK="https://github.com/jesseduffield/lazygit/$ARC_PATH"
+    curl -Lo lazygit.tar.gz $LINK
+    tar xf lazygit.tar.gz lazygit
+    install lazygit /usr/local/bin
+    rm -f lazygit*
 }
 
 set_python() {
 	echo "Python setup..."
-	pip install pyTelegramBotAPI --break-system-packages 
+    apt install python3-pip -y
+	pip install pyTelegramBotAPI --break-system-packages
 }
 
 set_golang() {
@@ -60,37 +69,59 @@ set_golang() {
 	wget "https://go.dev/dl/$version" && rm -rf /usr/local/go
 	tar -C /usr/local -xzf "$version" && rm "$version"
 	# path setup
-	PATH=$PATH:/usr/local/go/bin 
+	PATH=$PATH:/usr/local/go/bin
 	settings="/root/.bashrc"
 	setting="PATH=\$PATH:/usr/local/go/bin"
-	grep -q "^$setting" "$settings" || echo "$setting" >> "$settings" 
+	grep -q "^$setting" "$settings" || echo "$setting" >> "$settings"
 }
 
 set_vim() {
 	echo "Vim setup..."
+    curl -sL install-node.vercel.app/lts | bash
 	settings="/root/.vimrc"
-	setting_arr=("set colorcolumn=80" "match ErrorMsg '\%>80v.\+'")
+    setting_arr=(
+        "set expandtab"
+        "set tabstop=4"
+        "set shiftwidth=4"
+        "set smarttab"
+        "set smartindent"
+        ""
+        "set number"
+        "set cursorline"
+        "set colorcolumn=80"
+        "match ErrorMsg '\%>80v.\+'"
+        ""
+        "call plug#begin()"
+        "Plug 'vim-airline/vim-airline'"
+        "Plug 'neoclide/coc.nvim', {'branch': 'release'}"
+        "Plug 'preservim/nerdtree'"
+        "Plug 'nordtheme/vim'"
+        "call plug#end()"
+        ""
+        "colorscheme nord"
+    )
+    echo "" > $settings
 	for setting in "${setting_arr[@]}"; do
-		if ! grep -q "^$setting" "$settings"; then
-			echo "$setting" >> "$settings" 
-		fi
+			echo "$setting" >> "$settings"
 	done
+	echo "Don't forget to :PlugInstall in Vim"
 }
 
 set_user() {
 	echo "User setup..."
-	adduser $user
-	usermod -aG sudo $user
+	adduser $USER
+	usermod -aG sudo $USER
 	ssh-keygen -t ed25519
-	positive="PermitRootLogin yes"
+    positive="PermitRootLogin yes"
 	negative="PermitRootLogin no"
 	sed -i "s/$positive/$negative/g" /etc/ssh/sshd_config
 	systemctl restart sshd
 }
 
 install_deps() {
-	apt update && apt upgrade -y
-	apt install ufw cron git wget python3-pip vim tmux htop figlet -y 
+    apt update && apt upgrade -y
+    apt install ufw cron git lazygit wget -y
+    apt install vim tmux htop figlet -y
 }
 
 install_deps
